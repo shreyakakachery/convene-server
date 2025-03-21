060f1a7d92a0011b5eb4f7514a71247d80517645
Shreya Kakachery <shreyakakachery@gmail.com>
2025-03-21 12:01:44 -0700

    Add new overpass mirror url, getPlaces accepts any category

--- controllers/placesController.js
+++ controllers/placesController.js
@@ -1,47 +1,53 @@
 import axios from "axios";
 
 export async function getPlaces(req, res) {
-  const { lat, lon, radius = 500 } = req.query;
+  const { lat, lon, radius = 500, category = "cafe" } = req.query;
 
   if (!lat || !lon) {
-    return res.status(400).json({ error: "Latitude and longitude are required" });
+    return res
+      .status(400)
+      .json({ error: "Latitude and longitude are required" });
   }
 
-  // const query = `
-  //   [out:json];
-  //   node
-  //     ["amenity"="cafe"]
-  //     (around:${radius},${lat},${lon});
-  //   out;
-  // `;
-
   const query = `
-  [out:json];
-  (
-    node["amenity"="cafe"](around:${radius},${lat},${lon});
-    node["amenity"="restaurant"](around:${radius},${lat},${lon});
-  );
-  out;
-`;
+    [out:json];
+    node
+      ["amenity"="${category}"]
+      (around:${radius},${lat},${lon});
+    out center;
+  `;
 
-  const url = "https://overpass-api.de/api/interpreter";
+
+  // const url = "https://overpass-api.de/api/interpreter"; // slow mirror, used by many
+
+  const url = "https://overpass.kumi.systems/api/interpreter"; // faster, usa mirror
+
+  // https://overpass.openstreetmap.fr/api/interpreter // japan mirror
+
+  // https://overpass.nchc.org.tw/api/interpreter // germany mirror
 
   try {
-    const response = await axios.post(url, `data=${encodeURIComponent(query)}`, {
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    });
+    const response = await axios.post(
+      url,
+      `data=${encodeURIComponent(query)}`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Accept-Encoding": "gzip",
+        },
+      }
+    );
 
-    const places = response.data?.elements.map((place) => ({
-      id: place.id,
-      name: place.tags.name || "Name Not Found",
-      lat: place.lat || "Unknown",
-      lon: place.lon || "Unknown",
-      number: place.tags['addr:housenumber'] || "Unknown",
-      street: place.tags['addr:street'] || "Address Not Found",
-      amenity: place.tags.amenity  || "Unknown",
-      indoor_seating: place.tags.indoor_seating  || "Unknown",
-      outdoor_seating: place.tags.outdoor_seating  || "Unknown",      
-    })) || [];
+    const places =
+      response.data?.elements.map((place) => ({
+        id: place.id,
+        name: place.tags.name || "Name Not Found",
+        lat: place.lat || "Unknown",
+        lon: place.lon || "Unknown",
+        number: place.tags["addr:housenumber"] || "Unknown",
+        street: place.tags["addr:street"] || "Address Not Found",
+        amenity: place.tags.amenity || "Unknown",
+      })) || [];
 
     res.json(places);
   } catch (error) {
